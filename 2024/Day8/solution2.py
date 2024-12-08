37c7d493adb85fd3d2468fc13241da05a70b87c5
Peter Nybergh <peter.w.nybergh@gmail.com>
2024-12-08 08:50:44 +0100

    Add solution to day 8 2024

---
+++ 2024/Day8/solution2.py
@@ -0,0 +1,55 @@
+with open("data.txt", 'r') as data:
+
+    antenna_coords = {}
+    height = 0
+    for y, lines in enumerate(data.readlines()):
+        width = len(lines)
+        for x, char in enumerate(lines.strip()):
+            if char == '.':
+                continue
+            
+            if char in antenna_coords:
+                antenna_coords[char].append((x,y))
+            else:
+                antenna_coords[char] = [(x,y)]
+        height += 1
+    
+    def within_map(height, width, x, y):
+        return x >= 0 and x < width and y >= 0 and y < height
+
+    resonance_coords = set()
+    for antenna_set in antenna_coords.values():
+        for i, antenna1 in enumerate(antenna_set):
+            for j, antenna2 in enumerate(antenna_set[i:]):
+                if antenna1 == antenna2:
+                    continue
+                x_dist, y_dist = antenna1[0] - antenna2[0], antenna1[1] - antenna2[1]
+
+                # add the antennas too
+                resonance_coords.add(antenna1)
+                resonance_coords.add(antenna2)
+
+                i = 1
+                while True:
+                    resonance1 = (antenna1[0] + i * x_dist, antenna1[1] + i * y_dist)
+                    if within_map(height, width, resonance1[0], resonance1[1]):
+                        resonance_coords.add(resonance1)
+                    else:
+                        break
+                    i += 1
+
+                j = 1    
+                while True:
+                    resonance2 = (antenna2[0] - j * x_dist, antenna2[1] - j * y_dist)
+                    if within_map(height, width, resonance2[0], resonance2[1]):
+                        resonance_coords.add(resonance2)
+                    else:
+                        break
+                    j += 1
+
+with open("data.txt", 'r') as data:
+    area = [list(line.strip()) for line in data.readlines()]
+    for coord in resonance_coords:
+        area[coord[1]][coord[0]] = '#'
+    print('\n'.join([''.join(a) for a in area]))
+print(len(resonance_coords))
