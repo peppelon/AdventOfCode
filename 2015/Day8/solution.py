5a7a43f920d31c9329a207b5f52943e2eca5d4e7
Peter Nybergh <peter.w.nybergh@gmail.com>
2024-12-08 23:45:54 +0100

    Add solution part 2 to day 8 2015

--- 2015/Day8/solution.py
+++ 2015/Day8/solution.py
@@ -1,1 +1,2 @@
 print(sum(len(s.strip()) - len(eval(s)) for s in open('data.txt')))
+print(sum(len(s.strip().replace("\\","\\\\").replace("\"", "\\\"")) + 2 - len(s.strip()) for s in open('data.txt')))
